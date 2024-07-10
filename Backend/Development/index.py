0c79a2c8fea707925605782f1e79802ca3226ebc
masakazu-hirano <63486456+masakazu-hirano@users.noreply.github.com>
2024-07-10 09:56:35 +0900

    環境変数（.env）を読み込むための処理を実装

--- Backend/Development/index.py
+++ Backend/Development/index.py
@@ -1,4 +1,16 @@
 import logging
+import os
+
+from dotenv import load_dotenv
+
+def Read_Environment_File() -> bool:
+	directory: str = f"{os.environ['HOME']}/Documents/Projects/Personal_Dev"
+	return load_dotenv(
+		dotenv_path = f"{directory}/Backend/Development/.env",
+		encoding = 'utf-8',
+		override = True,
+		verbose = True
+	)
 
 if __name__ == '__main__':
 	logging.basicConfig(
@@ -7,4 +19,7 @@ if __name__ == '__main__':
 		style = '{'
 	)
 
-	logging.info(msg = '処理が正常に終了しました。')
+	if Read_Environment_File() == True:
+		logging.info(msg = '処理が正常に終了しました。')
+	else:
+		logging.error(msg = '環境変数（.env）ファイルの読み込みに失敗しました。')
