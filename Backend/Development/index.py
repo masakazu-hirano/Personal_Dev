0ec8970943e0577674ce236064a1421a282c1fd3
平野 雅一 <63486456+masakazu-hirano@users.noreply.github.com>
2024-09-04 08:20:30 +0900

    Python ファイルを新規作成

--- Backend/Development/index.py
+++ Backend/Development/index.py
@@ -1,16 +1,4 @@
 import logging
-import os
-
-from dotenv import load_dotenv
-
-def Read_Environment_File() -> bool:
-	directory: str = f"{os.environ['HOME']}/Documents/Projects/Personal_Dev"
-	return load_dotenv(
-		dotenv_path = f"{directory}/Backend/Development/.env",
-		encoding = 'utf-8',
-		override = True,
-		verbose = True
-	)
 
 if __name__ == '__main__':
 	logging.basicConfig(
@@ -19,7 +7,4 @@ if __name__ == '__main__':
 		style = '{'
 	)
 
-	if Read_Environment_File() == True:
-		logging.info(msg = '処理が正常に終了しました。')
-	else:
-		logging.error(msg = '環境変数（.env）ファイルの読み込みに失敗しました。')
+	logging.info(msg = '処理が正常に終了しました。')
