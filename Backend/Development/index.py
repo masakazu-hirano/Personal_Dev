56dc2c1e532ecb20a63a7770fb3b40f7885a6e80
平野 雅一 <63486456+masakazu-hirano@users.noreply.github.com>
2024-09-06 16:09:03 +0900

    保存する際のファイル名を作成

--- Backend/Development/index.py
+++ Backend/Development/index.py
@@ -1,6 +1,9 @@
 import logging
 import os
 
+from datetime import datetime
+from PIL import Image
+
 if __name__ == '__main__':
 	logging.basicConfig(
 		level = logging.INFO,
@@ -9,8 +12,18 @@ if __name__ == '__main__':
 	)
 
 	directory_name: str = './Development/Before_Edit_Picture'
-	for image_file in os.listdir(path = directory_name):
-		if os.path.isfile(path = f"{directory_name}/{image_file}"):
-			logging.info(msg = image_file)
+	for in_directory_file in os.listdir(path = directory_name):
+		if os.path.isfile(path = f"{directory_name}/{in_directory_file}"):
+			with Image.open(
+				fp = f"{directory_name}/{in_directory_file}",
+				mode = 'r',
+				formats = ('BMP', 'GIF', 'JPEG', 'PNG', 'WEBP')
+			) as image_file:
+				photography_datetime: datetime = datetime.strptime(image_file.getexif().get(key = 306), '%Y:%m:%d %H:%M:%S')
+				maker: str = image_file.getexif().get(key = 271)
+				model_name: str = image_file.getexif().get(key = 272)
 
+				logging.info(msg = f"{datetime.strftime(photography_datetime, '%Y-%m-%d %H:%M:%S')}_{maker}_{model_name}")
+
+		break
 	logging.info(msg = '処理が正常に終了しました。')
